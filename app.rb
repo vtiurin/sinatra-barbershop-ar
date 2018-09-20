46f50d6ab9ef11814c669c1b19ffa58455f47d77
Vladimir Tiurin <bfg.tyurin@gmail.com>
2018-09-20 03:00:28 +0300

    add Client entity

--- app.rb
+++ app.rb
@@ -1,6 +1,12 @@
 require 'rubygems'
 require 'sinatra'
+require 'sinatra/activerecord'
 require 'sinatra/reloader' if development?
+
+set :database, 'sqlite3:barbershop.db'
+
+class Client < ActiveRecord::Base
+end
 
 get '/' do
     erb 'recent posts:'
